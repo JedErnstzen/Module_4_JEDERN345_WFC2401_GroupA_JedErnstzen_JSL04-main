06404ed5fd5668072fad6ee2d7cbab5d2c71ce87
JedErnstzen <156651019+JedErnstzen@users.noreply.github.com>
2024-03-22 16:44:09 +0200

    Finished ternary operators and comments

--- index.js
+++ index.js
@@ -5,14 +5,14 @@
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
+console.log(userPreference ? (userPreference === 'Sugar-free' ? 'Sugar-free' : 'Regular') : 'Regular');
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
-let cansLeft = 3; // Any number of cans
-console.log(``);
+let cansLeft = 3; // Any number of cans that are left
+console.log(cansLeft < 5 ? 'Time to restock!' : 'We\'re stocked!');
 
 
 // #### Challenge 3: Workout Intensity
@@ -20,15 +20,16 @@ console.log(``);
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
 
 let heartRate = 95; // Current heart rate in bpm
-console.log(`'}`);
+console.log(heartRate < 100 ? 'Boost needed!' : 'Energy levels are high!');
+
 
 
 // #### Challenge 4: Temperature Suitability
 // Monster Energy drinks are best served cold. Given the current temperature, use a ternary to print "Chilled to perfection!" 
 // if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
 
-let currentTemp = 4; // Current temperature in °C
-console.log(``);
+let currentTemp = 4; // Current temperature in Celcius
+console.log(currentTemp <= 5 ? 'Chilled to perfection!' : 'Needs a cooler!');
 
 
 // #### Challenge 5: Late Night Coding Session
@@ -37,14 +38,12 @@ console.log(``);
 // otherwise, print "Better stick to water."
 
 // Define the current hour in 24-hour format
-let currentHour = 22; 
+let currentHour = 22; // Current hour in 24-hour format
+console.log(currentHour >= 7 && currentHour <= 24 ? 'Unleash the beast!' : 'Better stick to water.'); 
 
 // Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
 // If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
 // Otherwise, print "Better stick to water."
-
-
-
 
 
 
